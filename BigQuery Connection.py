c586223fbb87317f58dda2bd9ffdd6beebb46e06
Miguel Angel <63972784+MiguelAngel9604@users.noreply.github.com>
2021-12-29 23:51:25 -0600

    Join between lighthouse and BQ

--- BigQuery Connection.py
+++ BigQuery Connection.py
@@ -9,6 +9,12 @@
 from google.cloud.bigquery.client import Client
 from google.cloud import bigquery
 import os
+import json
+import os
+import pandas as pd
+import time
+from datetime import datetime
+from os.path import join
 
 #Credentials
 credential_path  ="C:\\Users\\mlope\\OneDrive\\Documentos\\Lighthouse\\Lighthouse_working\\dbtkey.json"
@@ -24,58 +30,94 @@ table_id_output = 'dbt-project-335000.dbt_mangel.output2'
 job_config = bigquery.QueryJobConfig(destination=table_id_output)#parameters = destination table to write 
 job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND #If the table already exists, then append the rows
 
+#Variables
+#df = pd.DataFrame([], columns=['fetch_time','site_url','site_id','user_agent','emulated_as'])
+df = pd.DataFrame([], columns=['URL','SEO','Accessibility','Performance','Best Practices'])
+name = "Report_reduce_version" 
+getdate = datetime.now().strftime("%m-%d-%y")
+relative_path = 'C:\\Users\\mlope\\OneDrive\\Documentos\\Lighthouse\\Lighthouse_working\\assets\\'  ### WINDOWS -> \\..\\..\\
+
 #Select URLs from input table
-df = client.query('''
+df_input_table = client.query('''
   SELECT 
     * 
   FROM `dbt-project-335000.dbt_mangel.input_test`''').result().to_dataframe()
 
-#print(df.head())
+def extract_info(preset):
+    global df
+    for ind in df_input_table.index:
+    # print(df['url'][ind], df['id'][ind])   
+        url = df_input_table['url'][ind]
+        stream = os.popen('lighthouse --disable-storage-reset=true --preset=' +
+                          preset + ' --output=json --output-path='+relative_path + name+'_'+getdate+'.report.json ' + url)
 
-for url in df['url']:
-        print('INFO URLs:'+url) 
+        time.sleep(60)
+        print("INFO:Report complete for: " + url+' !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
+        json_filename = join(relative_path+name+ '_' +getdate + '.report.json ')
 
+        with open(json_filename, encoding="utf8") as json_data:
+            loaded_json = json.load(json_data)
 
+        seo = str(round(loaded_json["categories"]["seo"]["score"] * 100))
+        accessibility = str(round(loaded_json["categories"]["accessibility"]["score"] * 100))
+        performance = str(round(loaded_json["categories"]["performance"]["score"] * 100))
+        best_practices = str(round(loaded_json["categories"]["best-practices"]["score"] * 100))
 
+        print("INFO:"+ seo + '!!!!!!!!!!!!!!!!!!!!!!!!!')
+        print("INFO:"+ accessibility + '!!!!!!!!!!!!!!!!!!!!!!!!!')
+        print("INFO:"+ performance + '!!!!!!!!!!!!!!!!!!!!!!!!!')
+        print("INFO:"+ best_practices + '!!!!!!!!!!!!!!!!!!!!!!!!!')
 
+        dict = {"URL":url,"SEO":seo,"Accessibility":accessibility,"Performance":performance,"Best Practices":best_practices}
+        
+        df = df.append(dict, ignore_index=True).sort_values(by='SEO', ascending=False)
+        #print('INFO: df' +df+ ' !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    df.to_csv(relative_path + name + '_' + getdate + '.csv')
+    print(df)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Get the values from the input table
-sql = """
-    SELECT 
-    * 
-  FROM `dbt-project-335000.dbt_mangel.input_test`;
-"""
-
-# Start the query, passing in the extra configuration.
-#query_job = client.query(sql, job_config=job_config)  # Make an API request.
-#query_job.result()  # Wait for the job to complete.
+extract_info(preset='desktop')
 
 
-print("Query results loaded to the table {}".format(table_id_output))
 
-datasets = list(client.list_datasets())  # Make an API request.
-project = client.project
 
-if datasets:
-    print("Datasets in project {}:".format(project))
-    for dataset in datasets:
-        print("\t{}".format(dataset.dataset_id))
-else:
-    print("{} project does not contain any datasets.".format(project))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##Get the values from the input table
+#sql = """
+#    SELECT 
+#    * 
+#  FROM `dbt-project-335000.dbt_mangel.input_test`;
+#"""
+#
+## Start the query, passing in the extra configuration.
+##query_job = client.query(sql, job_config=job_config)  # Make an API request.
+##query_job.result()  # Wait for the job to complete.
+#
+#
+#print("Query results loaded to the table {}".format(table_id_output))
+#
+#datasets = list(client.list_datasets())  # Make an API request.
+#project = client.project
+#
+#if datasets:
+#    print("Datasets in project {}:".format(project))
+#    for dataset in datasets:
+#        print("\t{}".format(dataset.dataset_id))
+#else:
+#    print("{} project does not contain any datasets.".format(project))
